3db6b671a9eb4c17ccd8050a17a8382fffb01ea1
SSK0206 <coccinelle.sasa@gmail.com>
2019-05-21 16:08:33 +0900

    インデント修正

--- resources/views/files/create.blade.php
+++ resources/views/files/create.blade.php
@@ -3,18 +3,18 @@
 @section('content')
 <div class="container">
     <div class="col-md-6">
-      <h4 style="margin-bottom:50px;">投稿</h4>
-      <form action="/create" method="POST" enctype="multipart/form-data">
-          {{ csrf_field() }}
-          <div class="form-group">
-            <input type="file" name="file" class="form-control-file" required>
-          </div>
-          <div class="form-group">
-            <label for="file_name">ファイル名</label>
-            <input type="text" name="file_name" class="form-control" required>
-          </div>
-          <input type="submit" value="投稿" class="btn btn-primary" id="upload">
-      </form>
+		<h4 style="margin-bottom:50px;">投稿</h4>
+		<form action="/create" method="POST" enctype="multipart/form-data">
+			{{ csrf_field() }}
+			<div class="form-group">
+				<input type="file" name="file" class="form-control-file" required>
+			</div>
+			<div class="form-group">
+				<label for="file_name">ファイル名</label>
+				<input type="text" name="file_name" class="form-control" required>
+			</div>
+			<input type="submit" value="投稿" class="btn btn-primary" id="upload">
+		</form>
     </div>
 </div>
 
